13e960a96ee4de8d8aec801d9a5acd03eb76c032
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-06-01 12:28:39 +0300

    sss

---
+++ index.js
@@ -0,0 +1,20 @@
+// Завдання 1
+
+const result = 5 + 5 + '5';
+console.log(result); 
+console.log(typeof result); 
+
+console.log("")
+
+// Завдання 2
+
+const My = 'My';
+const Name = 'name';
+const Is = 'is';
+let fullName = `${My} ${Name} ${Is} Viktor`;
+console.log(fullName); 
+
+console.log("")
+// Завдання 3
+
+// Завдання 4
