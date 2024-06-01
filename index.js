a3e21a70c4857ae255acd961b9799f6d5227f8dc
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-06-01 13:50:13 +0300

    HW

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 // Завдання 1
-
-const result = 5 + 5 + '5';
+const result = 5 + 5 + "5";
 console.log(result); 
 console.log(typeof result); 
 
 console.log("")
 
 // Завдання 2
+const email = "vlad.lipinkiy@gmail.com";
+const containsAt = email.includes("@");
+const length = email.length;
+console.log(containsAt); 
+console.log(length); 
 
-const My = 'My';
-const Name = 'name';
-const Is = 'is';
-let fullName = `${My} ${Name} ${Is} Viktor`;
+console.log("")
+
+// Завдання 3
+const word1 = "My";
+const word2 = "name";
+const word3 = "is";
+const fullName = word1 + " " + word2 + " " + word3 + " " + "Viktor";
 console.log(fullName); 
 
 console.log("")
-// Завдання 3
 
 // Завдання 4
+const userName = "Олександро";
+const payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
+
+console.log("")      
